f5c6273eb78b6b4bb3c2c30d9800134ff875d8c4
Shariq Khan <shariq.a.khan@uconn.edu>
2020-12-04 07:57:33 -0500

    added california county case no.

--- scrape.py
+++ scrape.py
@@ -1,29 +1,37 @@
 from selenium.webdriver import Chrome
-from bs4 import BeautifulSoup
 
 import time
 import pandas as pd
 
 with Chrome("chromedriver.exe") as driver:
-	driver.get("https://www.courts.ca.gov/cms/slapp.htm")
+    driver.get("https://www.courts.ca.gov/cms/slapp.htm")
 
-	counties = driver.find_element_by_name("slapp")
-	counties = [county.get_attribute("value") for county in counties.find_elements_by_tag_name("option") if county.get_attribute("value") != "X"]
+    counties = driver.find_element_by_name("slapp")
+    counties = [
+        county.get_attribute("value")
+        for county in counties.find_elements_by_tag_name("option")
+        if county.get_attribute("value") != "X"
+    ]
 
-	county_cases = {}
+    county_cases = {}
 
-	for county in counties:
-		driver.find_element_by_xpath("//select[@name='slapp']/option[text()='{}']".format(county)).click()
-		driver.find_element_by_name("Submit").submit()
+    for county in counties:
+        driver.find_element_by_xpath(
+            "//select[@name='slapp']/option[text()='{}']".format(county)
+        ).click()
+        driver.find_element_by_name("Submit").submit()
 
-		table_len = len(driver.find_elements_by_xpath(".//table/tbody/tr"))
+        table_len = len(driver.find_elements_by_xpath(".//table/tbody/tr"))
 
-		cases = []
-		for i in range(4, table_len, 16):
-			case_num = driver.find_elements_by_xpath(".//table/tbody/tr[{}]/td[2]".format(i))[0].text
-			cases.append(case_num)
-		
-		county_cases[county] = cases
+        cases = []
+        for i in range(4, table_len, 16):
+            case_num = driver.find_elements_by_xpath(
+                ".//table/tbody/tr[{}]/td[2]".format(i)
+            )[0].text
 
-		data = pd.DataFrame(cases, columns=[county])
-		data.to_csv("data/{}.csv".format(county))
+            cases.append(case_num)
+			
+        county_cases[county] = cases
+
+        data = pd.DataFrame(cases, columns=[county])
+        data.to_csv("data/{}.csv".format(county), index=False)
